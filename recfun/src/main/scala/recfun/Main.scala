530e4d1e123bb7c6dfefbe5482f250309fdde116
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-09-18 20:56:56 +0200

    Ex1 & Ex2 done

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,13 +13,25 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c < 0 || c > r) 0
+    else if (c == 0 && r == 0) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    def balance(chars: List[Char]): Boolean = {
+      def test(cur: Integer, open: Integer, close: Integer): Boolean =
+        if (cur >= chars.length) (open == close)
+        else if (chars(cur) == ')') if (open <= close) false else test(cur + 1, open, close + 1)
+        else if (chars(cur) == '(') test(cur + 1, open + 1, close)
+        else test(cur + 1, open, close)
+
+      test(0, 0, 0)
+    }
+
+
   /**
    * Exercise 3
    */
