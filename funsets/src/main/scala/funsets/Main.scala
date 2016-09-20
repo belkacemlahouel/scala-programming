f4a2d9e68808505a0d0d49adf5d486e4d072bf1f
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-09-20 23:07:55 +0200

    Reformatting and simplifying boolean expressions

--- funsets/src/main/scala/funsets/Main.scala
+++ funsets/src/main/scala/funsets/Main.scala
@@ -4,14 +4,18 @@ import FunSets._
 
 object Main extends App {
 
-  println(contains(singletonSet(1), 1) == true)
-  println(contains(singletonSet(2), 1) == false)
-  println(contains(union(singletonSet(1), singletonSet(2)), 3) == false)
-  println(contains(union(singletonSet(1), singletonSet(2)), 2) == true)
-  println(contains(intersect(singletonSet(1), singletonSet(2)), 10) == false)
-  println(contains(intersect(singletonSet(1), singletonSet(1)), 1) == true)
-  println(contains(diff(singletonSet(1), singletonSet(2)), 2) == false)
-  println(contains(diff(singletonSet(1), singletonSet(2)), 1) == true)
-  println(contains(filter(singletonSet(1), (x: Int) => x+x > 2), 1) == false)
-  println(contains(filter(singletonSet(1), (x: Int) => x+x == 2), 1) == true)
+  println(!contains(singletonSet(2), 1))
+  println(contains(singletonSet(1), 1))
+
+  println(!contains(union(singletonSet(1), singletonSet(2)), 3))
+  println(contains(union(singletonSet(1), singletonSet(2)), 2))
+
+  println(!contains(intersect(singletonSet(1), singletonSet(2)), 10))
+  println(contains(intersect(singletonSet(1), singletonSet(1)), 1))
+
+  println(!contains(diff(singletonSet(1), singletonSet(2)), 2))
+  println(contains(diff(singletonSet(1), singletonSet(2)), 1))
+
+  println(!contains(filter(singletonSet(1), (x: Int) => x+x > 2), 1))
+  println(contains(filter(singletonSet(1), (x: Int) => x+x == 2), 1))
 }
