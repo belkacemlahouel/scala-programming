86b0edb3b592a8498357cc547b5230cc32a40558
belkalah-mbp <belkacem.lahouel@gmail.com>
2016-09-20 22:53:07 +0200

    basic functions on sets + test in main

--- funsets/src/main/scala/funsets/Main.scala
+++ funsets/src/main/scala/funsets/Main.scala
@@ -1,6 +1,17 @@
 package funsets
 
+import FunSets._
+
 object Main extends App {
-  import FunSets._
-  println(contains(singletonSet(1), 1))
+
+  println(contains(singletonSet(1), 1) == true)
+  println(contains(singletonSet(2), 1) == false)
+  println(contains(union(singletonSet(1), singletonSet(2)), 3) == false)
+  println(contains(union(singletonSet(1), singletonSet(2)), 2) == true)
+  println(contains(intersect(singletonSet(1), singletonSet(2)), 10) == false)
+  println(contains(intersect(singletonSet(1), singletonSet(1)), 1) == true)
+  println(contains(diff(singletonSet(1), singletonSet(2)), 2) == false)
+  println(contains(diff(singletonSet(1), singletonSet(2)), 1) == true)
+  println(contains(filter(singletonSet(1), (x: Int) => x+x > 2), 1) == false)
+  println(contains(filter(singletonSet(1), (x: Int) => x+x == 2), 1) == true)
 }
